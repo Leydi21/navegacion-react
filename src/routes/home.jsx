d9646ee777864f0b8d2b89e9f49987d26d96879a
Martin Cruz M <codemcm@hotmail.com>
2022-05-29 13:15:23 -0500

    Forms in Home

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,15 +1,29 @@
 import React, { Component }  from 'react';
-import { Button, Container  } from 'react-bootstrap';
+import { Form, Button, Container  } from 'react-bootstrap';
 
 export default function Home() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Home</h2>
-        <Container>
-        <Button variant="primary" size="lg">
-    Block level button
+        <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
   </Button>
-  </Container>
+</Form>
       </main>
       
     );
